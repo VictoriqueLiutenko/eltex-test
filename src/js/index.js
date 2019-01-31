f44c23766e504e9aa3c60207979ca77e27baf078
Victorique <victorique@Victoriques-MacBook-Pro.local>
2019-01-31 14:57:03 +0200

    Add scrollwatch

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,7 @@
 const ScrollWatch = require("scrollwatch");
-const swInstance = new ScrollWatch({});
+
+const swInstance = new ScrollWatch({
+  onElementInView: function(data) {
+    console.log(data.el, "...is now in view");
+  }
+});
